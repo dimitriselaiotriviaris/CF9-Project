2cd1b40a41f6a24dfc1441bca0c108c23248ee49
Dimitris <dimitris.elaiotriviaris@gmail.com>
2026-07-01 14:32:49 +0300

    Add project files.

---
+++ CF9Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CF9Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
